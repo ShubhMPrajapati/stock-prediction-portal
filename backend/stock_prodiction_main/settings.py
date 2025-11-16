2538ba59e33a59c1f5079dd7501ae6700e6ff5ea
Shubham Prajapati <shubhamprajapati051@gmail.com>
2025-11-15 19:42:35 -0500

    backend file fixed

--- backend/stock_prodiction_main/settings.py
+++ backend/stock_prodiction_main/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+# D:\React\stock-prediction-portal\stockenv\Scripts\python.exe
 
 from pathlib import Path
 from decouple import config
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
